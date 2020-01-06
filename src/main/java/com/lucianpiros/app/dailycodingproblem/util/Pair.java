d2b5783379c5fee871a0e1db006e7fb9e899e90e
Lucian Piros <pirosl@yahoo.com>
2020-01-06 14:35:06 +0000

    Add Pair class

---
+++ src/main/java/com/lucianpiros/app/dailycodingproblem/util/Pair.java
@@ -0,0 +1,17 @@
+/**
+ * Generic Pair class
+ */ 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class Pair<T,S> {
+	private	T first;
+	private S second;
+
+	public Pair(T first, S second) {
+		this.first = first;
+		this.second = second;
+	}
+}
